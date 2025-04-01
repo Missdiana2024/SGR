d6f32c0351ee63cb7019f025d8388b408b2665f1
Spyware <spyware@Spyware.spyware.com>
2025-04-01 21:56:40 +0300

    Update DB connection for Aiven

--- sgr_db_connect.php
+++ sgr_db_connect.php
@@ -1,16 +1,12 @@
 <?php
-$host = "sgrdb-sgrnotificationdb.g.aivencloud.com";       // Database server
-$port = "14283";            // Port you're using for PostgreSQL
-$dbname = "sgr_notification_system";   // Database name
-$username = "avnadmin";     // Database username
-$password = "AVNS_TMT7VxP2bkdpVepWhV2";     // Database password
-
-try {
-    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $username, $password);
-    // Set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected to PostgreSQL SGR NOTIFICATION SYSTEM DATABASE successfully!";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+$host = "my-mysql-franknokwe100.aivencloud.com";
+$port = "12345";
+$dbname = "defaultdb";
+$user = "avnadmin";
+$pass = "your-secret-password";
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected to Aiven database successfully";
 ?>
